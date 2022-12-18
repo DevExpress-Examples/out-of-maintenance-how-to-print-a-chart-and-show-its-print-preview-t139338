afe5c739e327f399cd0129b381b09d02eaa53632
DevExpressExampleBot <fakeEmail>
2022-12-18 20:16:10 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,20 +1,20 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace PrintChart
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub button_ShowPrintPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			chartControl1.ShowPrintPreview(Me)
-		End Sub
+        Private Sub button_ShowPrintPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.chartControl1.ShowPrintPreview(Me)
+        End Sub
 
-		Private Sub button_Print_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			chartControl1.Print()
-		End Sub
-	End Class
+        Private Sub button_Print_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.chartControl1.Print()
+        End Sub
+    End Class
 End Namespace
